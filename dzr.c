3b24b62f9c4567c7e4ef419a1b1ba2b3c7910ed0
yne <yne@users.noreply.github.com>
2019-10-17 09:18:33 +0200

    Fix for -Wextra

--- dzr.c
+++ dzr.c
@@ -16,8 +16,8 @@
 #include <arpa/inet.h>
 
 #define closesocket close
-#define WSACleanup() 0
-#define WSAStartup(A, B) 0
+#define WSACleanup() ;
+#define WSAStartup(A, B) ;
 #endif
 
 #include <openssl/ssl.h>
@@ -34,9 +34,11 @@
 #define USAGE "USAGE:\n"\
               "  dzr [TRACK...]\n"\
               "ENVIRONMENT:\n"\
-              "  - FMT: MP3_128 = 0, MP3_320 = 3, AAC_96 = 8, FLAC=9\n"\
-              "  - sid=%s: your session id cookie\n"\
-              "  - DEBUG=1: enable debug mode\n"\
+              "  - DZR_SID=%s\n"\
+              "  - DZR_AES=%s\n"\
+              "  - DZR_CBC=%s\n"\
+              "  - DZR_FMT=%s (opt) one of 128K:0, 320K:3, AAC96:8, FLAC:9\n"\
+              "  - DEBUG=1: (opt)\n"\
               "EXEMPLES:\n"\
               "  sid=fr123456789 dzr 600629 # sid via local env\n"\
               "  dzr deezer.com/en/track/526893582 # url is ok\n"\
@@ -61,7 +63,7 @@ char *find(char *buf, const char *pattern) {
 }
 
 int debug(int color, int size, void *buf) {
-	//if (getenv("DEBUG"))fprintf(stderr, "\033[%i;1m%*.*s\033[0m", color, size, size, (char *) buf);
+	if (getenv("DEBUG"))fprintf(stderr, "\033[%i;1m%*.*s\033[0m", color, size, size, (char *) buf);
 	return size;
 }
 
@@ -80,8 +82,9 @@ char *toHex(unsigned char *bin, size_t count, char *hex, char *fmt) {
 }
 
 int fetch(char *meth, char *path, char *params, char *host, char **fields, char **bodies, char *out, size_t outmax,
-          int(*cb)(SSL*, ssize_t, void**), void**cb_args) {
-	int clen = 0, len = 0, tcp = socket(AF_INET, SOCK_STREAM, 0);
+          int(*cb)(SSL*, size_t, void**), void**cb_args) {
+	int len = 0, tcp = socket(AF_INET, SOCK_STREAM, 0);
+	size_t clen = 0;
 	expect(!connect(tcp, (const struct sockaddr *) &((struct sockaddr_in) {
 	 .sin_family = AF_INET, .sin_port = htons(443), .sin_addr.s_addr = *(in_addr_t *) (gethostbyname(host)->h_addr)
 	}), sizeof(struct sockaddr_in)));
@@ -95,7 +98,7 @@ int fetch(char *meth, char *path, char *params, char *host, char **fields, char 
 	char bodylen[9];
 	sprintf(bodylen, "%i", len);
 	char *header[] = {meth, " ", path, params ? params : "", " HTTP/1.0\r\n", "Host: ", host, "\r\n", "Content-Length: ", bodylen, "\r\n", 0};
-	for (int i = 0; header && header[i]; i++) http_send(ssl, header[i]);
+	for (int i = 0; header[i]; i++) http_send(ssl, header[i]);
 	for (int i = 0; fields && fields[i]; i++) http_send(ssl, fields[i]);
 	http_send(ssl, "\r\n");
 	for (int i = 0; bodies && bodies[i]; i++) http_send(ssl, bodies[i]);
@@ -108,7 +111,7 @@ int fetch(char *meth, char *path, char *params, char *host, char **fields, char 
 	if (cb) {
 		cb(ssl, clen, cb_args);
 	} else {
-		int pos = 0;
+		size_t pos = 0;
 		while (pos < outmax && http_recv(ssl, out + pos + len, 1) > 0)
 			++pos;
 		out[pos + len] = '\0';//TODO : rework
@@ -161,7 +164,7 @@ BF_KEY *getTrackKey(char *trackid, const char*bf, BF_KEY *bf_key) {
 	return bf_key;
 	//char dbg[128]={};toHex(xor, dbg, sizeof(xor), "%02x");fprintf(stderr, "decrypting with: %s\n", dbg);
 }
-int decryptTrack(SSL* ssl, ssize_t mp3_size, void**args) {
+int decryptTrack(SSL* ssl, size_t mp3_size, void**args) {
 	BF_KEY *bf_key = args[0];
 	FILE *fd = args[1];
 	unsigned char chunk[2048];
@@ -172,7 +175,7 @@ int decryptTrack(SSL* ssl, ssize_t mp3_size, void**args) {
 		if (!(nbchunk++ % 3)) {
 			BF_cbc_encrypt(chunk, chunk, sizeof(chunk), bf_key, (unsigned char[]) {0, 1, 2, 3, 4, 5, 6, 7}, BF_DECRYPT);
 		}
-		if (fwrite(chunk, sizeof(chunk), 1, fd) < 0)break;
+		if (fwrite(chunk, sizeof(chunk), 1, fd) != 1)break;
 		fprintf(stderr, "%c %zi\r", "/-\\|"[nbchunk / 512 % 4], total);
 		if (total + sizeof(chunk) > mp3_size) { // incomplete last chunk
 			for (ssize_t remain = mp3_size - total; remain > 0 && http_recv(ssl, chunk, 1) == 1; remain--)
@@ -180,8 +183,7 @@ int decryptTrack(SSL* ssl, ssize_t mp3_size, void**args) {
 			break;
 		}
 	}
-	fprintf(stderr, "\n");
-
+	return fprintf(stderr, "\n");
 }
 int fetchTrack(BF_KEY *bf_key, char *path, char cdnId, FILE *fd) {
 	char page[1024 * 32], cdn[24 + 1];
@@ -194,9 +196,9 @@ int fetchTrack(BF_KEY *bf_key, char *path, char cdnId, FILE *fd) {
 int main(int argc, char *argv[]) {
 	argv++, argc--;
 	char *sid = getenv("DZR_SID")?:getenv("sid")?:getenv("SID");
-	char *dbg = getenv("DEBUG"), *bf=getenv("DZR_CBC"), *aes=getenv("DZR_AES"), *fmt = getenv("FMT") ?: "0";
+	char *bf = getenv("DZR_CBC"), *aes=getenv("DZR_AES"), *fmt = getenv("DZR_FMT") ?: "0";
 	if (argc < 1 || !sid || !bf || !aes)
-		return fprintf(stderr, USAGE, sid, bf, aes);
+		return fprintf(stderr, USAGE, sid, aes, bf, fmt);
 	WSAStartup(MAKEWORD(2, 2), &(WSADATA) {});
 	SSL_library_init();
 
@@ -204,15 +206,15 @@ int main(int argc, char *argv[]) {
 	AES_set_encrypt_key((unsigned char *) aes, 128, &aes_key);
 
 	char *token = getTokenFromSID(sid, (char[32 + 1]) {});
+	//if (dbg)fprintf(stderr, "token=%s\n", token);
 
-	if (debug)fprintf(stderr, "sid=%s FMT=%s token=%s\n", sid, fmt, token);
 	for (char *track = *argv; argc; --argc, track = *++argv) {
 		for (; *track && !isdigit(*track); track++);//shift arg up to it the first digit
 		char md5[32 + 1] = {}, artist[64] = {}, title[64] = {};
 		int version = getTrackInfo(sid, token, track, md5, artist, title);
 		if(!*md5){continue;}/* private/blocked track */
 		int artist_len = (int) (strchr(artist, '"') - artist), title_len = (int) (strchr(title, '"') - title);
-		if(debug)fprintf(stderr, "version=%i md5=%s : %.*s - %.*s\n", version, md5, artist_len, artist, title_len, title);
+		//if(dbg)fprintf(stderr, "version=%i md5=%s : %.*s - %.*s\n", version, md5, artist_len, artist, title_len, title);
 		char *url = getTrackUrl(md5, version, track, fmt, (char[160 + 1]) {}, &aes_key);
 		FILE *fd = stdout;
 		if (isatty(STDOUT_FILENO)) { // TTY == no STDOUT piping => output to a pre-named file
