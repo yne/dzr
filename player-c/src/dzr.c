592ca9adff2b7f58750d1c17a1df4d322ad199ca
PitchHybrid <messias_junior@outlook.com>
2025-01-30 14:46:53 -0300

    curl, cjson and cmake add

--- player-c/src/dzr.c
+++ player-c/src/dzr.c
@@ -1,11 +1,6 @@
-#ifndef DZR_H
-#define DZR_H
-
 #include "dzr.h"
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-
+#include <cjson/cJSON.h>
 
 struct window_t {
     WINDOW *window;
@@ -14,7 +9,7 @@ struct window_t {
     int x;
     int starty;
     int startx;
-    char *label;
+    char label[15];
 };
 
 void create_win(window_t *w);
@@ -32,31 +27,40 @@ void type_search(window_t *win, char *str);
 
 char * search_input(window_t *win);
 
-int http_get(char *url, void *response_data);
+void * http_get(char *url);
 
 // https://pubs.opengroup.org/onlinepubs/7908799/xcurses/intovix.html
 
 
 int main(void) { // int argc, char **argv
     
-    INIT_CURSES();
+    init_curses();
+
+    buffer_t * response = (buffer_t *) http_get("https://api.deezer.com/search/playlist?q=HARDBASS");
+
+    cJSON *json = cJSON_ParseWithLength(response->data, response->size);
+    LOG("%s", cJSON_Print(json));
+
+    cJSON *data = cJSON_GetObjectItem(json, "data");
+    cJSON *list = cJSON_GetArrayItem(data, 0);
+    cJSON *id = cJSON_GetObjectItem(list, "id");
 
     window_t *playlist_w = calloc(1, sizeof(window_t));
-    playlist_w->label = "Playlist";
+    strcpy(playlist_w->label, "Playlist");
     playlist_w->y = layout->yMax - 3;
     playlist_w->x = layout->xDiv;
     playlist_w->starty = 0;
     playlist_w->startx = 0;
 
     window_t *painel_w = calloc(1, sizeof(window_t));
-    painel_w->label = "Painel";
+    strcpy(painel_w->label, "Painel");
     painel_w->y = layout->yDiv;
     painel_w->x = layout->xMax - layout->xDiv;
     painel_w->starty = 0;
     painel_w->startx = layout->xDiv;
     
     window_t *search_w = calloc(1, sizeof(window_t));
-    search_w->label = "Search";
+    strcpy(search_w->label, "Search");
     search_w->y = 3;
     search_w->x = painel_w->x / 3;
     search_w->starty = 1;
@@ -186,29 +190,61 @@ int main(void) { // int argc, char **argv
     return 0;
 }
 
-size_t write_data(void *ptr, size_t size, size_t nmemb, void* stream) {
+size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream) {
     size_t total_size = size * nmemb;
-    strncat(stream, ptr, total_size);
+    buffer_t *b = (buffer_t *) stream;
+    char * tmp = realloc(b->data, b->size + total_size + 1);
+    if(!tmp) {
+        return 0;
+    }
+
+    b->data = tmp;
+    memcpy(&b->data[b->size], ptr, total_size);
+    b->size += total_size;
+    b->data[b->size] = 0;
     return total_size;
 }
+void * http_get(char *url){
+    
+    buffer_t *response_data = malloc(sizeof(buffer_t));
+    response_data->data = malloc(1);
+    response_data->size = 0;
 
-int http_get(char *url, void *response_data){
+    curl_global_init(CURL_GLOBAL_ALL);
     CURL *curl = curl_easy_init();
     if(curl) {
         curl_easy_setopt(curl, CURLOPT_URL, url);
 
         char *proxy = getenv("HTTP_PROXY");
         if(proxy) {
-            int atPos = strcspn(proxy, "@");
-            printf("proxy: %s\n", proxy);
-            printf("atPos: %d\n", atPos);
-            printf("proxy: %s\n", proxy + atPos);
+            char *at = strchr(proxy, '@');
+            char *userpwd = NULL;
+            char *host = NULL;
+            char *port = NULL;
 
-            // curl_easy_setopt(curl, CURLOPT_PROXY, proxy + atPos);
-            // curl_easy_setopt(curl, CURLOPT_PROXYUSERPWD, proxy);
-            
-            // curl_easy_setopt(curl, CURLOPT_PROXY, "172.31.136.14:128");
-            // curl_easy_setopt(curl, CURLOPT_PROXYUSERPWD, "manoel.messias:jeremias");
+            if(at) {
+                userpwd = strndup(proxy, at - proxy);
+                char *hostport = at + 1;
+                char *colon = strchr(hostport, ':');
+
+                if(colon) {
+                    host = strndup(hostport, colon - hostport);
+                    port = colon + 1;
+
+                    curl_easy_setopt(curl, CURLOPT_PROXY, host);
+                    curl_easy_setopt(curl, CURLOPT_PROXYPORT, atoi(port));
+                } else {
+                    curl_easy_setopt(curl, CURLOPT_PROXY, hostport);
+                }
+                curl_easy_setopt(curl, CURLOPT_PROXYUSERPWD, userpwd);
+            } else {
+                curl_easy_setopt(curl, CURLOPT_PROXY, proxy);
+            }
+
+            free(userpwd);
+            free(host);
+            free(port);
+            free(proxy);
         }
         #ifdef SKIP_PEER_VERIFICATION
             curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
@@ -218,7 +254,6 @@ int http_get(char *url, void *response_data){
         #endif
 
         curl_easy_setopt(curl, CURLOPT_CA_CACHE_TIMEOUT, 604800L);
-        
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, response_data);
         CURLcode res = curl_easy_perform(curl);
@@ -229,15 +264,29 @@ int http_get(char *url, void *response_data){
 
         curl_easy_cleanup(curl);
     }
-    return 0;
+    curl_global_cleanup();
+    return (void *) response_data;
 }
 
-void clear_and_write(window_t * w, char *str) {
+void clear_and_write(window_t *w, char *str) {
+    if (w == NULL || str == NULL || w->window == NULL) {
+        return;
+    }
+
     wclear(w->window);
-    box(w->window, 0, 0);
-    addLabel( w->window, w->label);
-    mvwaddstr(w->window, 1, 3, str);
-    wrefresh(w->window);
+    if (box(w->window, 0, 0) == ERR) {
+        return;
+    }
+
+    addLabel(w->window, w->label);
+
+    if (mvwaddstr(w->window, 1, 3, str) == ERR) {
+        return;
+    }
+
+    if (wrefresh(w->window) == ERR) {
+        return;
+    }
 }
 
 char * search_input(window_t *w) {
@@ -251,7 +300,7 @@ char * search_input(window_t *w) {
     return track;
 }
 
-void type_search(window_t *win, char * str) {
+void type_search(window_t *win, char str[]) {
     int i = 0;
     int c;
     while ((c = getch()) != '\n') {
@@ -279,22 +328,43 @@ void create_win(window_t *w) {
 }
 
 void addLabel(WINDOW *win, char *str) {
+    if (win == NULL || str == NULL) {
+        return;
+    }
+
     size_t len = strlen(str);
     char *nstr = malloc(len + 3);
+    if (nstr == NULL) {
+        return;
+    }
+
     sprintf(nstr, " %.*s ", (int)len, str);
     int x = getmaxx(win) * 0.10;
-    mvwaddstr(win, 0, x, nstr);
-    wrefresh(win);
+    if (mvwaddstr(win, 0, x, nstr) == ERR) {
+        return;
+    }
+    if (wrefresh(win) == ERR) {
+        return;
+    }
     free(nstr);
 }
 
 void destroy_menu(window_t *w, ITEM **items) {
-    if(w->menu != NULL) {
-        unpost_menu(w->menu);
-        free_menu(w->menu);
-        for(int i = 0; items[i] != NULL; i++) {
-            free_item(items[i]);
-        }
+    if (w == NULL || w->menu == NULL) {
+        return;
+    }
+
+    unpost_menu(w->menu);
+    free_menu(w->menu);
+    w->menu = NULL;
+
+    if (items == NULL) {
+        return;
+    }
+
+    for (int i = 0; items[i] != NULL; i++) {
+        free_item(items[i]);
+        items[i] = NULL;
     }
 }
 
@@ -318,61 +388,69 @@ void drive_menu(window_t *w, int key) {
     refresh();
 }
 
-void logging(char t, char *str, ...) {
-   
-    static window_t *logging;
+static void logging(char t, char *str, ...) {
+    static window_t *logging = NULL;
 
-    static window_t *command;
+    static window_t *command = NULL;
 
-    if(logging == NULL) {
+    if (str == NULL) {
+        if (logging != NULL) {
+            free_window(logging);
+            logging = NULL;
+            LOG("Destroyed logging window\n");
+        }
+        if (command != NULL) {
+            free_window(command);
+            command = NULL;
+            LOG("Destroyed command window\n");
+        }
+        return;
+    }
+
+    if (logging == NULL) {
         logging = calloc(1, sizeof(window_t));
-        logging->label = "Logging";
+        if (logging == NULL) {
+            LOG("Unable to allocate memory for logging window\n");
+            return;
+        }
+        strcpy(logging->label, "Logging");
         logging->y = layout->yMax - layout->yDiv;
         logging->x = layout->xMax - layout->xDiv;
         logging->starty = layout->yDiv;
         logging->startx = layout->xDiv;
     }
 
-    if(command == NULL) {
+    if (command == NULL) {
         command = calloc(1, sizeof(window_t));
-        command->label = "Command";
+        if (command == NULL) {
+            LOG("Unable to allocate memory for command window\n");
+            return;
+        }
+        strcpy(command->label, "Command");
         command->y = layout->yMax - layout->yDiv;
         command->x = layout->xDiv;
         command->starty = layout->yDiv;
         command->startx = 0;
     }
-    if (str == NULL) {
-        if(logging->window != NULL) {
-            free_window(logging);
-            LOG("Destroyed logging window");
-        }
-        if(command->window != NULL) {
-            free_window(command);
-            LOG("Destroyed command window");
-        }
-        return;
-    } else {
-        if(logging->window == NULL) {
-            create_win(logging);
-            LOG("Created logging window");
-        }
-        if(command->window == NULL) {
-            create_win(command);
-            LOG("Created command window");
-        }
 
-    }
     va_list args;
     va_start(args, str);
 
     char buffer[256];
     vsnprintf(buffer, sizeof(buffer), str, args);
     va_end(args);
-
-    if(t == 'C') {
+    if (t == 'C') {
+        if (command->window == NULL) {
+            create_win(command);
+            LOG("Created command window\n");
+        }
         clear_and_write(command, buffer);
     }
-    if(t == 'L') {
+    if (t == 'L') {
+        if (logging->window == NULL) {
+            create_win(logging);
+            LOG("Created logging window\n");
+        }
         clear_and_write(logging, buffer);
     }
     
@@ -381,11 +459,11 @@ void logging(char t, char *str, ...) {
 }
 
 void free_window(window_t *w) {
-    if (w->window != NULL) {
-        delwin(w->window);
-        free(w->label);
+    if (w != NULL) {
+        if(w->window != NULL){
+            delwin(w->window);
+        }
         free(w);
     }
 }
 
-#endif // DZR_H
--- player-c/src/dzr.h
+++ player-c/src/dzr.h
@@ -1,3 +1,5 @@
+#ifndef DZR_H
+#define DZR_H
 
 #ifdef __linux__
 #include <form.h>
@@ -9,17 +11,20 @@
 #include <ncurses/form.h>
 #include <ncurses/menu.h>
 #include <ncurses/panel.h>
-#include <curl/curl.h>
 #endif
+#include <curl/curl.h>
 
-#define DEBUG
+#include <stdio.h>
+#include <stdlib.h>
 
-#ifdef DEBUG
-#define LOG(fmt, ...) \
-    do { \
-        fprintf(stderr, "DEBUG: " fmt "\n\n", ##__VA_ARGS__); \
-        fflush(stderr); \
-    } while (0)
+#define DEBUG 1
+
+#if DEBUG
+#define LOG(fmt, ...)                                                          \
+  do {                                                                         \
+    fprintf(stderr, "DEBUG: " fmt "\n", ##__VA_ARGS__);                        \
+    fflush(stderr);                                                            \
+  } while (0)
 #else
 #define LOG(fmt, ...)
 #endif
@@ -28,42 +33,46 @@
 
 #define CTRL_D 4
 
-#define NCURSES_TRACE 1
-
-void logging(char t, char *str, ...);
+static void logging(char t, char *str, ...);
 
 #define LOGGING(str, ...) logging('L', str, ##__VA_ARGS__);
 #define COMMAND(str, ...) logging('C', str, ##__VA_ARGS__);
 
-#define INIT_CURSES()                                                          \
-    do {                                                                       \
-        initscr();                                                             \
-        noecho();                                                              \
-        raw();                                                                 \
-        keypad(stdscr, TRUE);                                                  \
-        layout = (screen_layout_t *)calloc(1, sizeof(screen_layout_t));        \
-        getmaxyx(stdscr, layout->yMax, layout->xMax);                          \
-        layout->xDiv = layout->xMax / 4;                                       \
-        layout->yDiv = layout->yMax - 3;                                       \
-        LOGGING("");                                                           \
-    } while (0);                                                               \
-
 #define CHECK_WINDOW(x)                                                        \
-    do {                                                                       \
-        if (!(x)) {                                                            \
-            endwin();                                                          \
-            LOG("Unable to create window %s", #x);                             \
-            exit(1);                                                           \
-        }                                                                      \
-    } while (0);                                                               \
+  do {                                                                         \
+    if (!(x)) {                                                                \
+      endwin();                                                                \
+      LOG("Unable to create window %s", #x);                                   \
+      exit(1);                                                                 \
+    }                                                                          \
+  } while (0);
 
 typedef struct window_t window_t;
 
 typedef struct {
-    int xMax;
-    int yMax;
-    int xDiv;
-    int yDiv;
+  int xMax;
+  int yMax;
+  int xDiv;
+  int yDiv;
 } screen_layout_t;
 
+typedef struct {
+    char * data;
+    size_t size;
+} buffer_t;
+
 screen_layout_t *layout;
+
+static inline void init_curses() {
+  initscr();
+  noecho();
+  raw();
+  keypad(stdscr, TRUE);
+  layout = (screen_layout_t *) calloc(1, sizeof(screen_layout_t));
+  getmaxyx(stdscr, layout->yMax, layout->xMax);
+  layout->xDiv = layout->xMax / 4;
+  layout->yDiv = layout->yMax - 3;
+  LOGGING("");
+  COMMAND("");
+}
+#endif // DZR_H
